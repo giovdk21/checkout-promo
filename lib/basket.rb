032dcc5ce55cde6a3b04dff657d90820dd4298ab
Giovanni Derks <giovdk21@gmail.com>
2017-02-22 21:45:25 +0000

    Add basket checkout
    - add item to basket
    - find item by code

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,3 +1,26 @@
+# Shopping basket / checkout manager
 class Basket
-  # placeholder
+  attr_reader :items_count
+
+  def initialize
+    @items = []
+    @items_count = 0
+  end
+
+  # Add an item to the basket, incrementing its quantity if already present
+  def scan(item)
+    found_item = find_by_code(item.code)
+    if found_item
+      found_item[:quantity] += 1
+    else
+      @items << { data: item, quantity: 1 }
+    end
+
+    @items_count += 1
+  end
+
+  # Find an item in the basket by its product code
+  def find_by_code(code)
+    @items.detect { |item| item[:data].code == code }
+  end
 end
