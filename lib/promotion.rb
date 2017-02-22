6c6ba0e4b4e57c51680d1c1b064c34fd308ff2f0
Giovanni Derks <giovdk21@gmail.com>
2017-02-22 20:56:41 +0000

    Implement method to get promotion rules

--- lib/promotion.rb
+++ lib/promotion.rb
@@ -1,4 +1,11 @@
 # Manage / retrieve promotions
 class Promotion
-  # placeholder
+  # Get the rule set for the required promotion calling the
+  # promotion API endpoint
+  def get_rules_for(promotion_name)
+    response = Net::HTTP.get(
+      'api.localhost', "/v1/promotion?id=#{promotion_name}"
+    )
+    JSON.parse(response)
+  end
 end
